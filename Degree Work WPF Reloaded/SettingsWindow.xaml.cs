2429afda329fa8987da3ec7d9ecce8caf429484d
KostenkoNikita <neutralunit1187@gmail.com>
2017-02-22 02:53:06 +0200

    Save Window v2

--- Degree Work WPF Reloaded/SettingsWindow.xaml.cs
+++ Degree Work WPF Reloaded/SettingsWindow.xaml.cs
@@ -24,17 +24,46 @@ namespace Degree_Work
         enum ThicknessTarget { Line, Vector, BorderStroke }
         ColorTarget selectedColorTarget;
         ThicknessTarget selectedThicknessTarget;
+        IStreamLinesPlotWindow w;
+        delegate void VisualParamsChangedAsyncDelegate();
+        VisualParamsChangedAsyncDelegate d;
+        IAsyncResult res;
+
         byte R, G, B;
         double thickness;
 
-        public SettingsWindow()
+        public SettingsWindow(IStreamLinesPlotWindow _w)
         {
             InitializeComponent();
+            w = _w;
+            d = new VisualParamsChangedAsyncDelegate(w.OnPlotVisualParamsChanged);
             targetList.SelectionChanged += TargetList_SelectionChanged;
             targetList.SelectedIndex = 0;
             ThicknessSlider.ValueChanged += ThicknessSlider_ValueChanged;
             targetThicknessList.SelectionChanged += TargetThicknessList_SelectionChanged;
             targetThicknessList.SelectedIndex = 0;
+
+            hVertSlider.Value = Settings.PlotGeomParams.hVertical; hVertSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+            hHorSlider.Value = Settings.PlotGeomParams.MRKh; hHorSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+            xMaxSlider.Value = Settings.PlotGeomParams.XMax; xMaxSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+            xMinSlider.Value = Settings.PlotGeomParams.XMin; xMinSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+            yMaxSlider.Value = Settings.PlotGeomParams.YMax; yMaxSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+
+            if (w is HalfPlane)
+            {
+                yMinSlider.Value = yMinSlider.Maximum = 0; yMinSlider.IsEnabled = false;
+            }
+            else
+            {
+                yMinSlider.Value = Settings.PlotGeomParams.YMin; yMinSlider.ValueChanged += PlotParamsSlider_ValueChanged;
+            }
+
+            hVertOutputTextBlock.Text = hVertSlider.Value.ToString(Settings.Format);
+            hHorOutputTextBlock.Text = hHorSlider.Value.ToString(Settings.Format);
+            xMaxOutputTextBlock.Text = xMaxSlider.Value.ToString(Settings.Format);
+            xMinOutputTextBlock.Text = xMinSlider.Value.ToString(Settings.Format);
+            yMaxOutputTextBlock.Text = yMaxSlider.Value.ToString(Settings.Format);
+            yMinOutputTextBlock.Text = yMinSlider.Value.ToString(Settings.Format);            
         }
 
         private void TargetThicknessList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -42,9 +71,9 @@ namespace Degree_Work
             selectedThicknessTarget = (ThicknessTarget)targetThicknessList.SelectedIndex;
             switch (selectedThicknessTarget)
             {
-                case ThicknessTarget.Line: thickness = Settings.LineStrokeThickness; break;
-                case ThicknessTarget.Vector: thickness = Settings.ArrowStokeThickness; break;
-                case ThicknessTarget.BorderStroke: thickness = Settings.BorderStrokeThickness; break;
+                case ThicknessTarget.Line: thickness = Settings.PlotVisualParams.LineStrokeThickness; break;
+                case ThicknessTarget.Vector: thickness = Settings.PlotVisualParams.ArrowStokeThickness; break;
+                case ThicknessTarget.BorderStroke: thickness = Settings.PlotVisualParams.BorderStrokeThickness; break;
             }
             SetThicknessSliderValue();
             RSlider.Focus();
@@ -55,10 +84,10 @@ namespace Degree_Work
             selectedColorTarget = (ColorTarget)targetList.SelectedIndex;
             switch (selectedColorTarget)
             {
-                case ColorTarget.Line: R = Settings.LineColor.R; G = Settings.LineColor.G; B = Settings.LineColor.B; break;
-                case ColorTarget.Vector: R = Settings.ArrowColor.R; G = Settings.ArrowColor.G; B = Settings.ArrowColor.B; break;
-                case ColorTarget.BorderFill: R = Settings.BorderFillColor.R; G = Settings.BorderFillColor.G; B = Settings.BorderFillColor.B; break;
-                case ColorTarget.BorderStroke: R = Settings.BorderStrokeColor.R; G = Settings.BorderStrokeColor.G; B = Settings.BorderStrokeColor.B; break;
+                case ColorTarget.Line: R = Settings.PlotVisualParams.LineColor.R; G = Settings.PlotVisualParams.LineColor.G; B = Settings.PlotVisualParams.LineColor.B; break;
+                case ColorTarget.Vector: R = Settings.PlotVisualParams.ArrowColor.R; G = Settings.PlotVisualParams.ArrowColor.G; B = Settings.PlotVisualParams.ArrowColor.B; break;
+                case ColorTarget.BorderFill: R = Settings.PlotVisualParams.BorderFillColor.R; G = Settings.PlotVisualParams.BorderFillColor.G; B = Settings.PlotVisualParams.BorderFillColor.B; break;
+                case ColorTarget.BorderStroke: R = Settings.PlotVisualParams.BorderStrokeColor.R; G = Settings.PlotVisualParams.BorderStrokeColor.G; B = Settings.PlotVisualParams.BorderStrokeColor.B; break;
             }
             SetColorSliderValue();
             RSlider.Focus();
@@ -85,10 +114,10 @@ namespace Degree_Work
         {
             switch (selectedColorTarget)
             {
-                case ColorTarget.Line: Settings.LineColor = OxyColor.FromRgb(R,G,B); break;
-                case ColorTarget.Vector: Settings.ArrowColor = OxyColor.FromRgb(R, G, B); break;
-                case ColorTarget.BorderFill: Settings.BorderFillColor = OxyColor.FromRgb(R, G, B); break;
-                case ColorTarget.BorderStroke: Settings.BorderStrokeColor = OxyColor.FromRgb(R, G, B); break;
+                case ColorTarget.Line: Settings.PlotVisualParams.LineColor = OxyColor.FromRgb(R,G,B); break;
+                case ColorTarget.Vector: Settings.PlotVisualParams.ArrowColor = OxyColor.FromRgb(R, G, B); break;
+                case ColorTarget.BorderFill: Settings.PlotVisualParams.BorderFillColor = OxyColor.FromRgb(R, G, B); break;
+                case ColorTarget.BorderStroke: Settings.PlotVisualParams.BorderStrokeColor = OxyColor.FromRgb(R, G, B); break;
             }
         }
 
@@ -96,9 +125,9 @@ namespace Degree_Work
         {
             switch (selectedThicknessTarget)
             {
-                case ThicknessTarget.Line: Settings.LineStrokeThickness = thickness; break;
-                case ThicknessTarget.Vector: Settings.ArrowStokeThickness  = thickness; break;
-                case ThicknessTarget.BorderStroke: Settings.BorderStrokeThickness = thickness; break;
+                case ThicknessTarget.Line: Settings.PlotVisualParams.LineStrokeThickness = thickness; break;
+                case ThicknessTarget.Vector: Settings.PlotVisualParams.ArrowStokeThickness  = thickness; break;
+                case ThicknessTarget.BorderStroke: Settings.PlotVisualParams.BorderStrokeThickness = thickness; break;
             }
         }
 
@@ -112,6 +141,7 @@ namespace Degree_Work
             }
             FillRectangle();
             AssignColorValue();
+            res = d.BeginInvoke(null, null);
         }
 
         private void ThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -119,33 +149,25 @@ namespace Degree_Work
             thickness = ThicknessSlider.Value;
             ThicknessOutputTextBlock.Text = thickness.ToString(Settings.Format);
             AssignThicknessValue();
+            res = d.BeginInvoke(null, null);
         }
 
-
-
-
-
-
-
-
-
-
-
-
-        private void ico_MouseEnter(object sender, MouseEventArgs e)
+        private void PlotParamsSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            
+            Settings.PlotGeomParams.hVertical = hVertSlider.Value; hVertOutputTextBlock.Text = hVertSlider.Value.ToString(Settings.Format);
+            Settings.PlotGeomParams.MRKh = hHorSlider.Value; hHorOutputTextBlock.Text = hHorSlider.Value.ToString(Settings.Format);
+            Settings.PlotGeomParams.XMax = xMaxSlider.Value; xMaxOutputTextBlock.Text = xMaxSlider.Value.ToString(Settings.Format);
+            Settings.PlotGeomParams.XMin = xMinSlider.Value; xMinOutputTextBlock.Text = xMinSlider.Value.ToString(Settings.Format);
+            Settings.PlotGeomParams.YMax = yMaxSlider.Value; yMaxOutputTextBlock.Text = yMaxSlider.Value.ToString(Settings.Format);
+            Settings.PlotGeomParams.YMin = yMinSlider.Value; yMinOutputTextBlock.Text = yMinSlider.Value.ToString(Settings.Format);
+            w.OnPlotGeomParamsChanged();
         }
 
-        private void ico_MouseLeave(object sender, MouseEventArgs e)
-        {
-            
-        }
 
-        private void ico_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Close();
-        }
+
+
+
+
 
     }
 }
